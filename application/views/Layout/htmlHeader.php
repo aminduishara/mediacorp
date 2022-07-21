f8224ddae80b2b4ddabc777995160dc45ac64018
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-07-21 09:24:41 +0530

    Update the homeImpl page and img page

--- application/views/Layout/htmlHeader.php
+++ application/views/Layout/htmlHeader.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sample Form</title>
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/bootstrap/css/bootstrap.css">    
+    <script src="<?= base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="<?= base_url(); ?>assets/FormCss/form.css">
+    <script src="https://use.fontawesome.com/66fcf391bc.js"></script>
 </head>
 <body>
