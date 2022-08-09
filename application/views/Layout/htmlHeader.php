d66c25295e7fe3f5ef919eb39dd39714dc09ad2d
Amindu Ishara <aminduishara@gmail.com>
2022-08-09 13:07:24 +0530

    Changed priority

--- application/views/Layout/htmlHeader.php
+++ application/views/Layout/htmlHeader.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mediacorp Registration</title>
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/bootstrap/css/bootstrap.css">    
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/bootstrap/css/bootstrap.css">
     <script src="<?= base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?= base_url(); ?>assets/bootstrap/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/FormCss/form.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/FormCss/form.css">
 </head>
+
 <body>
