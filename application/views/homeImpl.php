64c96796e76fe9829cb1e1d6fedb0b3d11797876
Amindu Ishara <aminduishara@gmail.com>
2022-08-09 14:19:35 +0530

    Change ref no position

--- application/views/homeImpl.php
+++ application/views/homeImpl.php
@@ -2,7 +2,7 @@
 
 <div class="container pt-5 pb-5">
   <h3 class="text-center">General Information</h3>
-  <input class="float-end btn btn-sm btn-dark" id="refNo" value="0000" disabled>
+  <input class="float-end btn btn-sm btn-dark" id="refNo" value="0000" disabled style="margin-top: 1rem;">
   <input class="float-end btn btn-sm btn-dark" id="aplicentID" value="0" hidden>
 </div>
 <div class="container mb-5">
