9db15c9bf28b3aa0b5af68bc16c28060125afb7d
Maduranga91 <maduranga.cmaduranga.chirath@gmail.com>
2022-10-15 22:22:55 +0530

    update the img page 1.30

--- application/views/Forms/img.php
+++ application/views/Forms/img.php
@@ -124,7 +124,7 @@
                 var aplicentID = 12000; //document.getElementById("aplicentID").value;
                 var typeID = $('#ddlType').val();
 
-                alert($('#fileUpload').value);
+                alert($('#fileUpload').value == null);
 
                 if (aplicentID != "" && typeID != 0) {
                         // $(this).hide();
