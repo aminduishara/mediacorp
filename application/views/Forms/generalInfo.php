3d3fe83d9e89329dad9d3d218decf1cbe7745714
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-10-17 14:50:20 +0530

    Minor Change

--- application/views/Forms/generalInfo.php
+++ application/views/Forms/generalInfo.php
@@ -322,34 +322,15 @@
                             </div>
                         </div>
 
-                
-                    <?php
-
-                    } else if ($row->mas_reglable_id == 10) {
-                        if ($row->mas_reglable_visibility == 0) {
-                            $visibility = "none";
-                        } else {
-                            $visibility = "";
-                        }
-
-                        if ($row->mas_reglable_required == 1) {
-                            $status =  'data-req="1"';
-                            $requiredicon = ' <span class="text-danger">*</span>';
-                        } else {
-                            $status = 'data-req="0"';
-                            $requiredicon = '';
-                        } ?>
-
-
-                            <div class="col-sm-6 mt-4" style="display:<?php echo $visibility ?>">
-                                <div class="wrapper">
-                                    <div class="input-data">
-                                        <input type="date" name="Date" id="date" placeholder=none <?php echo $status; ?> value="<?php echo date('Y-m-d'); ?>" onchange="this.setAttribute('value', this.value);">
-                                        <div class="underline"></div>
-                                        <label><?php echo $row->mas_reglable_text ?><?php echo $requiredicon; ?></label>
-                                    </div>
+                        <div class="col-sm-6 mt-4">
+                            <div class="wrapper">
+                                <div class="input-data">
+                                    <input type="text" name="contact_person" id="contact_person" <?php echo $status; ?> onchange="this.setAttribute('value', this.value);">
+                                    <div class="underline"></div>
+                                    <label>Contact Person</label>
                                 </div>
-                            </div>        
+                            </div>
+                        </div>   
 
                 </div>
 
