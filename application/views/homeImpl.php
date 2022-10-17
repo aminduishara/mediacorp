343e7f352a2a4d4db7cf6357500cc3e03e0427c0
Amindu Ishara <aminduishara@gmail.com>
2022-10-17 12:29:50 +0530

    C

--- application/views/homeImpl.php
+++ application/views/homeImpl.php
@@ -11,9 +11,9 @@
   <div class="col-12">
     <div class="card">
       <div class="row" style="position: relative;">
-        <div class="svg" style="position: absolute; top: 0; left: -100px; height: 130px; width: 130px;">
+        <div class="svg" style="position: absolute; top: -2px; left: -120px; height: 130px; width: 130px;">
           <!-- <img src="<?php echo base_url(); ?>assets/Vector 1.svg" alt="side panel" > -->
-          <svg width="51" height="58" viewBox="0 0 51 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="130" height="130" viewBox="0 0 51 58" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M50.3098 49.5714C50.6048 56.1429 45 57 45 57V43C45 43 50.0148 43 50.3098 49.5714Z" stroke="black" stroke-width="0.5" />
             <path d="M14.5 36.5C14.2788 29.9286 21.9909 29 21.9909 29H45.0001V43H21.9909C21.9909 43 14.7212 43.0714 14.5 36.5Z" stroke="black" stroke-width="0.5" />
             <path d="M49.9888 21.5714C50.2837 28.1429 44.6789 29 44.6789 29V15C44.6789 15 49.6938 15 49.9888 21.5714Z" stroke="black" stroke-width="0.5" />
