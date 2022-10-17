3f4ecc2a7f70bbf65d0202bd09dba472b13a661c
Amindu Ishara <aminduishara@gmail.com>
2022-10-17 12:19:32 +0530

    C

--- application/views/homeImpl.php
+++ application/views/homeImpl.php
@@ -12,7 +12,7 @@
     <div class="card">
       <div class="row" style="position: relative;">
         <div class="svg">
-          <img src="<?php echo base_url(); ?>assets/Vector 1.svg" alt="side panel">
+          <img src="<?php echo base_url(); ?>assets/Vector 1.svg" alt="side panel" style="position: absolute; top: 0; left: 0;">
         </div>
         <div class="col-md-1 left-pane d-none d-md-flex d-lg-flex d-xl-flex d-xxl-flex">
           <ul class="nav nav-tabs" id="myTab" role="tablist">
