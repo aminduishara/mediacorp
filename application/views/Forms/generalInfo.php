0a0894be82e020fb2f113fcd805035ac6758a031
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-10-17 16:16:48 +0530

    Minor Changes

--- application/views/Forms/generalInfo.php
+++ application/views/Forms/generalInfo.php
@@ -659,7 +659,8 @@
                         $status = 'data-req="0"';
                         $requiredicon = '';
                     } ?>
-                        <div class="col-sm-6">
+
+                        <div class="col-sm-6" style="display:<?php echo $visibility ?>">
                             <div class="wrapper">
                                 <div class="input-data">
                                     <input type="number" name="No_Employees" id="no_employees" <?php echo $status; ?> value="" onchange="this.setAttribute('value', this.value);">
