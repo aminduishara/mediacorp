e294f5b3c45678978e0ecf6bed3d92e2b6b0f4d8
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-10-17 13:36:28 +0530

    New added Files

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://demo2022.e-valuate.net/Registration';
+$config['base_url'] = 'http://localhost:8099/InternProjects/mediacorp/';
 
 /*
 |--------------------------------------------------------------------------
