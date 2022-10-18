9019862bd59be5259e963587a79b45e89f05d08c
ChanulDeeraka <chanuldeeraka@gmail.com>
2022-10-18 12:53:21 +0530

    Change the button type

--- application/views/Forms/img.php
+++ application/views/Forms/img.php
@@ -48,7 +48,7 @@
                                                                                         <input class="form-control" type="file" id="ImgFile<?php echo $i + 1 ?>" onchange="preview(<?php echo $i + 1 ?>)">
                                                                                 </div>
                                                                                 <div class="col-md-2">
-                                                                                        <button class="btn btn-danger" onclick="clearImage(<?php echo $i + 1 ?>)"><i class="fa fa-remove"></i></button>
+                                                                                        <button type="button" class="btn btn-danger" onclick="clearImage(<?php echo $i + 1 ?>)"><i class="fa fa-remove"></i></button>
                                                                                 </div>
                                                                         </div>
                                                                 </div>
