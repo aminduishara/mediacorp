200f339144b19caf2ad3e46c9f2892c17f0b3e3d
Amindu Ishara <aminduishara@gmail.com>
2022-10-31 11:06:38 +0530

    C

--- application/views/homeImpl.php
+++ application/views/homeImpl.php
@@ -9,10 +9,14 @@
 <div class="container pt-5 pb-1 mb-5">
   <div class="col-12 text-end">
     <h3 class="text-center"><?php if (isset($para->mas_compara_header1)) echo $para->mas_compara_header1 . ' ' . $para->mas_compara_header2 . ' ' . $para->mas_compara_header3; ?></h3>
-    <div class="col-md-2 offset-md-10">
-      <label for="">Reference No:</label>
-      <input class="mt-1 form-control" id="refNo" value="0000" disabled>
-      <input id="aplicentID" value="-1" hidden>
+    <div class="col-md-5 offset-md-10">
+      <div class="row">
+        <label for="" class="col-md-3">Reference No:</label>
+        <div class="col-md-9">
+          <input class="mt-1 form-co ntrol" id="refNo" value="0000" disabled>
+          <input id="aplicentID" value="-1" hidden>
+        </div>
+      </div>
     </div>
   </div>
   <div class="col-12">
