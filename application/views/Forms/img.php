6c238fb93333e534306d3761c698a5631803891a
Amindu Ishara <aminduishara@gmail.com>
2022-11-11 17:25:46 +0530

    C

--- application/views/Forms/img.php
+++ application/views/Forms/img.php
@@ -1,18 +1,17 @@
 <style>
         .loading {
-                width: 93%;
+                width: 100%;
                 height: 93%;
                 background: rgba(255, 255, 255, 0.15);
-                box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
                 backdrop-filter: blur(4px);
                 -webkit-backdrop-filter: blur(4px);
                 border-radius: 10px;
-                border: 1px solid rgba(255, 255, 255, 0.18);
                 position: absolute;
                 z-index: 100;
                 display: flex;
                 align-items: center;
                 justify-content: center;
+                left: -10px;
         }
 
         .loading .container1 {
