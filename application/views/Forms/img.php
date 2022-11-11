53ba60b3f83fc2717950e684a7fda6c3e7fd77f7
Amindu Ishara <aminduishara@gmail.com>
2022-11-11 10:39:11 +0530

    C

--- application/views/Forms/img.php
+++ application/views/Forms/img.php
@@ -45,7 +45,7 @@
                                         </div>
                                 </div>
                                 <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" onclick="$('#modalView').modal('hide');">Close</button>
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close">Close</button>
                                 </div>
                         </div>
                 </div>
