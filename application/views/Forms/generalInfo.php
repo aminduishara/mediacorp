3d5b5d7837dbb78dcd1bd58c4be542a62813d958
Amindu Ishara <aminduishara@gmail.com>
2022-11-12 18:42:40 +0530

    C

--- application/views/Forms/generalInfo.php
+++ application/views/Forms/generalInfo.php
@@ -216,10 +216,6 @@
         </div>
     </form>
     <script>
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
         $(document).ready(function() {
 
             $.ajax({
