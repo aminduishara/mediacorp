a271720537ee812ea106645af1c5aacd0be4a209
Amindu Ishara <aminduishara@gmail.com>
2022-11-12 18:45:58 +0530

    C

--- application/views/Layout/htmlFooter.php
+++ application/views/Layout/htmlFooter.php
@@ -1,5 +1,9 @@
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://unpkg.com/@popperjs/core@2"></script>
+<script>
+    var exampleEl = document.getElementById('butViewEdit')
+    var tooltip = new bootstrap.Tooltip(exampleEl)
+</script>
 
 </html>
