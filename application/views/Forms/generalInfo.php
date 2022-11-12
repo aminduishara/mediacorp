f97abebc38c47a93712e2b9550951587f0a9cbe9
Amindu Ishara <aminduishara@gmail.com>
2022-11-12 17:52:38 +0530

    C

--- application/views/Forms/generalInfo.php
+++ application/views/Forms/generalInfo.php
@@ -16,7 +16,7 @@
                                 <label for="txtRefNo">Ref. No:</label>
                                 <input type="text" class="form-control" id="txtRefNo" name="txtRefNo" placeholder="Ref. No">
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-7">
                                 <label for="txtEmailAddress">Email Address:</label>
                                 <input type="text" class="form-control" id="txtEmailAddress" name="txtEmailAddress" placeholder="Email Address">
                             </div>
